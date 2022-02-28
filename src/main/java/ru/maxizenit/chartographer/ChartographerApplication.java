2c9f46de0308b6d59920a7f26fc0c9bbda4222bc
Maxim Puzanov <maxizenit@gmail.com>
2022-02-28 17:56:34 +0300

    Added creation of a content folder if it is missing

--- src/main/java/ru/maxizenit/chartographer/ChartographerApplication.java
+++ src/main/java/ru/maxizenit/chartographer/ChartographerApplication.java
@@ -3,14 +3,24 @@ package ru.maxizenit.chartographer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @SpringBootApplication
 public class ChartographerApplication {
 
   /** Путь к папке контента. */
   public static String contentPath;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     contentPath = args[0];
+    Path contentPathAsPathType = Path.of(contentPath);
+
+    if (!Files.exists(contentPathAsPathType)) {
+      Files.createDirectories(contentPathAsPathType);
+    }
+
     SpringApplication.run(ChartographerApplication.class, args);
   }
 }
